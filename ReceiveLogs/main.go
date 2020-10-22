068d8991bb586fdaa7be5cfcdcae56ec0d038a9b
zhangyongsheng <zysdio77@sina.com>
2020-10-22 10:48:29 +0800

    add docker-compose.yaml

--- ReceiveLogs/main.go
+++ ReceiveLogs/main.go
@@ -37,20 +37,26 @@ func main() {
 	r := gin.Default()
 	r.POST("/log/receive", func(c *gin.Context) {
 		//c.JSON(200,Ss{"asdfjasldfkj"})
+		job := c.Query("jobname")
+		//job := c.Param("name")
+		//fmt.Println(name,job,age)
 		body, err := ioutil.ReadAll(c.Request.Body) //把请求的Request.Body写入body，body为[]byte格式
 		if err != nil {
 			log.WithFields(log.Fields{
 				"key": "ioutil.ReadAll",
 			}).Error(err)
 		}
-		r, err := ff.Write(body) //把body内容写入到文件
+		b := job + ":" + string(body) + "\n"
+		//_, err = ff.Write(body) //把body内容写入到文件
+		resolt, err := ff.WriteString(b) //把字符串写入文件
+		//fmt.Println(r)
 		if err != nil {
 			c.String(2001, "Error:%V\n", err)
 			log.WithFields(log.Fields{
 				"key": "ff.Write(body)",
 			}).Error(err)
 		} else {
-			c.String(200, "%V ok\n", r)
+			c.String(200, "%v ok\n", resolt)
 			log.WithFields(log.Fields{
 				"key": "main.c.String",
 			}).Info("Write log file succussful\n")
